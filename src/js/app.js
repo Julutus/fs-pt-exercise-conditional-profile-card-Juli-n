4fccbd964997846bc573335f9901abd9e100f754
Julutus <julian94info@gmail.com>
2024-10-09 19:15:08 +0000

    Borrar nombre url

--- src/js/app.js
+++ src/js/app.js
@@ -48,22 +48,22 @@ function render(variables = {}) {
             <li><a href="${
               variables.twitter
                 ? variables.twitter
-                : "https://twitter.com/4geeksacademy"
+                : "https://twitter.com/"
             }"><i class="fab fa-twitter"></i></a></li>
             <li><a href="${
               variables.github
                 ? variables.github
-                : "https://github.com/4geeksacademy"
+                : "https://github.com/"
             }"><i class="fab fa-github"></i></a></li>
             <li><a href="${
               variables.linkedin
                 ? variables.linkedin
-                : "https://linkedin.com/school/4geeksacademy"
+                : "https://linkedin.com/in/"
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="${
               variables.instagram
                 ? variables.instagram
-                : "https://instagram.com/4geeksacademy"
+                : "https://instagram.com/"
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
